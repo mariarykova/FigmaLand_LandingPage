10e35a42172c3d0848223ca6360394ad35b49e67
Maria Rykova <maria.nigay@yandex.ru>
2023-03-01 21:40:34 +0400

    add gulp-gh-pages plugin

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ const rigger = require("gulp-rigger");
 const notify = require("gulp-notify");
 const cleanDist = require("gulp-clean");
 const browserSync = require("browser-sync").create();
-// const ghPages = require("gulp-gh-pages");
 const cleanCss = require("gulp-clean-css");
+const ghPages = require("gulp-gh-pages");
 
 /* Paths */
 
@@ -130,6 +130,10 @@ function watchFiles() {
 
 const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
 const watch = gulp.parallel(build, watchFiles, serve);
+
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*").pipe(ghPages());
+});
 
 exports.html = html;
 exports.css = css;
